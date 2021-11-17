408f0e194fa1d11c955fb4082bd6072f59e28bca
anapaula-sds <86493078+anapaula-sds@users.noreply.github.com>
2021-11-17 08:33:17 -0300

    Todas as alterações

---
+++ js/main.js
@@ -0,0 +1,89 @@
+let line = gets().split("Visita na Feira ");
+let A = parseInt(line[]);
+let B = parseInt(line[]);
+let total = A + B; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
+
+/*function clicou(){
+    document.getElementById("Agradecimento");
+    console.log(document.getElementById("Agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qaul sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert (soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"))
+*/
+
+/*var d = new Date();
+alert(d);
+*/
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count)
+}
+*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+/*var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+/*var frutas = [{nome: "maçã", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas[1].nome);
+*/
+
+/*var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva")
+////lista.pop();
+//console.log(lista.length)
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join("*"))
+
+//var n1 = 29;
+//var n2 = 29;
+//alert(nome + " tem" + idade + " anos");
+//var frase = "Japão é o melhor time do mundo";
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
+
